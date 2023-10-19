220bd3105ab5cc6e4e58b50d4aca284279f71183
Artur Myronchuk <122921226+ArturMyronchuk@users.noreply.github.com>
2023-10-19 20:13:53 +0300

    dz3

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+const mongoose = require("mongoose");
 const app = require("./app");
-
-const PORT = 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`);
-});
+const { error } = require("./schema");
+const PORT = 3000;
+//YqCe05JnYw7Ci5zH
+mongoose
+  //.connect(DB_HOST)
+  .connect(process.env.DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
